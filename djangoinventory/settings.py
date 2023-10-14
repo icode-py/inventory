d5cd09aa29f2f26eef6a278241939249655cbf96
icode-py <stinoemmanuel6@gmail.com>
2023-10-14 18:31:16 +0100

    added index page for staff,created staff and admin profile

--- djangoinventory/settings.py
+++ djangoinventory/settings.py
@@ -138,3 +138,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dashboard-index'
+LOGIN_URL='user-login'
--- user/views.py
+++ user/views.py
@@ -17,6 +17,9 @@ def register(request):
     }
     return render(request,'user/register.html',context)
 
+def profile(request):
+    return render(request,'user/profile.html')
+
 
 # def register(request):
 #     if request.method == 'POST':
